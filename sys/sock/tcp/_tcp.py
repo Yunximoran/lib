4865f34d98565e5a05ebfe4f3389082a27ba2712
yumo <2970368069@qq.com>
2025-04-02 20:11:07 +0800

    update

--- sys/sock/tcp/_tcp.py
+++ sys/sock/tcp/_tcp.py
@@ -4,8 +4,11 @@ import socket
 class _ProtoType:
     def __init__(self, address, listens):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.settings()
+        
         self.sock.bind(address)
         self.sock.listen(listens)
+        
     
     def settings(self):
         pass
